b8377ef8171788f3164269b58cfff17bbf6a8392
Amal Prakash <44752430+amalp12@users.noreply.github.com>
2022-01-22 22:22:28 +0300

    Update BST.c

--- C/DataStructures/Binary Search Tree/BST.c
+++ C/DataStructures/Binary Search Tree/BST.c
@@ -4,28 +4,28 @@
 #include <string.h>
 #define INT_MAX  2147483647
 typedef long int li;
-typedef struct tree_node * tree_node_ptr;
+typedef struct BST_node * BST_node_ptr;
 
 
-struct tree_node
+struct BST_node
 {
     int data;
-    tree_node_ptr left; // left child less than data
-    tree_node_ptr right; // right child greater than data
+    BST_node_ptr left; // left child less than data
+    BST_node_ptr right; // right child greater than data
 };
 
 
 
-tree_node_ptr create_node(int key)
+BST_node_ptr BST_create_node(int key)
 {
-    tree_node_ptr new_node = (tree_node_ptr) malloc(sizeof(struct tree_node));
+    BST_node_ptr new_node = (BST_node_ptr) malloc(sizeof(struct BST_node));
     new_node->data = key;
     new_node->right = NULL;
     new_node->left = NULL;
     return new_node;
 }
 
-void insert_tree_node(tree_node_ptr root, tree_node_ptr new_node)
+void BST_insert_node(BST_node_ptr root, BST_node_ptr new_node)
 {
     // if tree is empty put the current node as root node
     if (root ==NULL)
@@ -46,7 +46,7 @@ void insert_tree_node(tree_node_ptr root, tree_node_ptr new_node)
         // if the left child is not empty then recursively call the function insert tree node towards left
         else
         {
-            insert_tree_node(root->left, new_node); // pass by memory
+            BST_insert_node(root->left, new_node); // pass by memory
         }
 
     }
@@ -62,7 +62,7 @@ void insert_tree_node(tree_node_ptr root, tree_node_ptr new_node)
         // if the right child is not empty then recursively call the function insert tree node towards right
         else
         {
-            insert_tree_node(root->right, new_node); // pass by memory
+            BST_insert_node(root->right, new_node); // pass by memory
         }
     }
     else// if the new node is equal to the current node then do nothing
@@ -70,14 +70,14 @@ void insert_tree_node(tree_node_ptr root, tree_node_ptr new_node)
         return;
     }
 }
-tree_node_ptr search(tree_node_ptr  root, int key)
+BST_node_ptr BST_search(BST_node_ptr  root, int key)
 {
     // if the root is empty then return NULL
     if(root == NULL)
     {
         return NULL;
     }
-    tree_node_ptr ptr = root;
+    BST_node_ptr ptr = root;
     //if key is equal to the current node then return the current node
     if(ptr->data == key)
     {
@@ -86,12 +86,12 @@ tree_node_ptr search(tree_node_ptr  root, int key)
     // if the key is less than the current node then go left
     else if(key < ptr->data)
     {
-        return search(ptr->left, key);
+        return BST_search(ptr->left, key);
     }
     // if the key is greater than the current node then go right
     else if (key > ptr->data)
     {
-        return search(ptr->right, key);
+        return BST_search(ptr->right, key);
     }
     else
     {
@@ -99,7 +99,7 @@ tree_node_ptr search(tree_node_ptr  root, int key)
     }
 
 }
-tree_node_ptr get_parent( tree_node_ptr root, tree_node_ptr child)
+BST_node_ptr BST_get_parent( BST_node_ptr root, BST_node_ptr child)
 {
     // if the root is empty then return NULL
     if(root == NULL)
@@ -124,26 +124,26 @@ tree_node_ptr get_parent( tree_node_ptr root, tree_node_ptr child)
     // if the left child is not empty then recursively call the function get parent towards left
     if(child->data <root->data )
     {
-        return get_parent(root->left, child);
+        return BST_get_parent(root->left, child);
     }
     // if the right child is not empty then recursively call the function get parent towards right
     else if(child->data >root->data)
     {
-        return get_parent(root->right, child);
+        return BST_get_parent(root->right, child);
     }
     
 }
 
-tree_node_ptr get_sucessor(tree_node_ptr root, int data)
+BST_node_ptr BST_get_sucessor(BST_node_ptr root, int data)
 {
 
-    tree_node_ptr search_result = search(root, data);
+    BST_node_ptr search_result = BST_search(root, data);
     if(search_result!=NULL)
     {
         // if right subtree not null then  sucessor will be the leftmost child of hte right subtree or right child itself
         if(search_result->right != NULL)
         {
-            tree_node_ptr temp = search_result->right; ;
+            BST_node_ptr temp = search_result->right; ;
             while(temp->left != NULL)
             {
                 temp = temp->left;
@@ -153,8 +153,8 @@ tree_node_ptr get_sucessor(tree_node_ptr root, int data)
         }
         else // if right subtree is null sucessor will be the last left child's parent while searching the current node
         {
-            tree_node_ptr sucessor= root;
-            tree_node_ptr temp = root;
+            BST_node_ptr sucessor= root;
+            BST_node_ptr temp = root;
             while(temp->data != search_result->data)
             {
                 if (search_result->data > temp->data)
@@ -181,18 +181,18 @@ tree_node_ptr get_sucessor(tree_node_ptr root, int data)
 
 
 
-tree_node_ptr get_predecessor(tree_node_ptr root, int data)
+BST_node_ptr BST_get_predecessor(BST_node_ptr root, int data)
 {
   
 
 
-    tree_node_ptr search_result = search(root, data);
+    BST_node_ptr search_result = BST_search(root, data);
     if(search_result!=NULL)
     {
         // if left subtree not null then  sucessor will be the rightmost child of hte left subtree or left child itself
         if(search_result->left != NULL)
         {
-            tree_node_ptr temp = search_result->left; 
+            BST_node_ptr temp = search_result->left; 
             while(temp->right != NULL)
             {
                 temp = temp->right;
@@ -202,8 +202,8 @@ tree_node_ptr get_predecessor(tree_node_ptr root, int data)
         }
         else // if left subtree is null sucessor will be the last right child's parent while searching the current node
         {
-            tree_node_ptr predecessor = root;
-            tree_node_ptr temp = root;
+            BST_node_ptr predecessor = root;
+            BST_node_ptr temp = root;
             while(temp->data != search_result->data)
             {
                 if (search_result->data < temp->data)
@@ -229,7 +229,7 @@ tree_node_ptr get_predecessor(tree_node_ptr root, int data)
 
 }
 
-void delete_node(tree_node_ptr * root, int key)
+void BST_delete_node(BST_node_ptr * root, int key)
 {
     
     // if the root is empty then return
@@ -242,26 +242,26 @@ void delete_node(tree_node_ptr * root, int key)
         *root = NULL;
         return;
     }
-    tree_node_ptr search_result = search(*root, key);
+    BST_node_ptr search_result = BST_search(*root, key);
     if(search_result != NULL)
     {
-        tree_node_ptr parent = get_parent(*root, search_result);
+        BST_node_ptr parent = BST_get_parent(*root, search_result);
 
         // if parent is null then that means root node is to be deleted
         if (parent==NULL)
         {
-            tree_node_ptr sucessor = get_sucessor(*root, key);
+            BST_node_ptr sucessor = BST_get_sucessor(*root, key);
             if(sucessor != NULL)
             {
                 int temp = sucessor->data;
-                delete_node(root, sucessor->data);
+                BST_delete_node(root, sucessor->data);
                 search_result->data = temp;
             }
             else
             {
-                tree_node_ptr predecessor = get_predecessor(*root, key);        
+                BST_node_ptr predecessor = BST_get_predecessor(*root, key);        
                 int temp = predecessor->data;
-                delete_node(root, predecessor->data);
+                BST_delete_node(root, predecessor->data);
                 search_result->data = temp;
             }
         }
@@ -329,9 +329,9 @@ void delete_node(tree_node_ptr * root, int key)
         // if the node to be deleted has two children
         else
         {
-            tree_node_ptr successor = get_sucessor( *root, search_result->data );
+            BST_node_ptr successor = BST_get_sucessor( *root, search_result->data );
             int temp_data = successor->data;
-            delete_node (root, successor->data);
+            BST_delete_node (root, successor->data);
             // if the node to be deleted is the parent's left child
             if(parent->left == search_result)
             {
@@ -355,7 +355,7 @@ void delete_node(tree_node_ptr * root, int key)
     
 }
 
-void preorder(tree_node_ptr  current_node)
+void BST_preorder(BST_node_ptr  current_node)
 {
     if(current_node == NULL)
     {
@@ -363,48 +363,48 @@ void preorder(tree_node_ptr  current_node)
     }
 
     printf("%d ", current_node->data);
-    preorder(current_node->left);
-    preorder(current_node->right);
+    BST_preorder(current_node->left);
+    BST_preorder(current_node->right);
 }
 
-void postorder(tree_node_ptr  current_node)
+void BST_postorder(BST_node_ptr  current_node)
 {
     if(current_node == NULL)
     {
         return;
     }
 
-    postorder((current_node->left));
-    postorder((current_node->right));
+    BST_postorder((current_node->left));
+    BST_postorder((current_node->right));
     printf("%d ", current_node->data);
 }
 
 
-void inorder(tree_node_ptr current_node)
+void BST_inorder(BST_node_ptr current_node)
 {
     if(current_node == NULL)
     {
         return;
     }
 
-    inorder((current_node->left));
+    BST_inorder((current_node->left));
     printf("%d ", current_node->data);
-    inorder((current_node->right));
+    BST_inorder((current_node->right));
 }
-void insert(tree_node_ptr * root, int key)
+void BST_insert(BST_node_ptr * root, int key)
 {
     // if the root is empty then create a new node
     if(*root == NULL)
     {
-        (*root) = create_node(key);
+        (*root) = BST_create_node(key);
         return;
     }
-    tree_node_ptr new_node = create_node(key);
-    insert_tree_node(*root, new_node);
+    BST_node_ptr new_node = BST_create_node(key);
+    BST_insert_node(*root, new_node);
 }
-void sucessor(tree_node_ptr root, int data)
+void BST_sucessor(BST_node_ptr root, int data)
 {
-    tree_node_ptr sucessor = get_sucessor(root, data);
+    BST_node_ptr sucessor = BST_get_sucessor(root, data);
     if(sucessor!=NULL)
     {
         printf("%d\n", sucessor->data);
@@ -415,9 +415,9 @@ void sucessor(tree_node_ptr root, int data)
     }
 }
 
-void predecessor(tree_node_ptr root, int data)
+void BST_predecessor(BST_node_ptr root, int data)
 {
-    tree_node_ptr predecessor = get_predecessor(root, data);
+    BST_node_ptr predecessor = BST_get_predecessor(root, data);
     if(predecessor!=NULL)
     {
         printf("%d\n", predecessor->data);
@@ -428,12 +428,12 @@ void predecessor(tree_node_ptr root, int data)
     }
 }
 
-void delete(tree_node_ptr * root, int key)
+void BST_delete(BST_node_ptr * root, int key)
 {
-    tree_node_ptr search_result = search(*root, key);
+    BST_node_ptr search_result = BST_search(*root, key);
     if (search_result!=NULL)
     {
-        delete_node(root, key);
+        BST_delete_node(root, key);
         printf("%d\n", key);
     }
     else
@@ -444,7 +444,7 @@ void delete(tree_node_ptr * root, int key)
 int main()
 {
     char in; int data;
-    tree_node_ptr root = NULL,x,y; // initalizing head to null
+    BST_node_ptr root = NULL,x,y; // initalizing head to null
     while(1)
     {
         scanf(" %c", &in);
@@ -454,19 +454,19 @@ int main()
             case 'a':
 
                 scanf(" %d", &data);
-                insert(&root, data);
+                BST_insert(&root, data);
                 
                 
                 break;
             
             case 'd':
                 scanf(" %d", &data);
-                delete(&root, data);
+                BST_delete(&root, data);
                 break;
 
             case 's':
                 scanf(" %d", &data);
-                tree_node_ptr search_result= search(root, data);
+                BST_node_ptr search_result= BST_search(root, data);
                 if (search_result != NULL)
                 {
                     printf("1\n");
@@ -479,28 +479,28 @@ int main()
 
             case 'c':
                 scanf(" %d", &data);
-                sucessor(root, data);
+                BST_sucessor(root, data);
                 
                 break;
             case 'r':
                 scanf(" %d", &data);
-                predecessor(root, data);
+                BST_predecessor(root, data);
 
                 break;
             case 'i':
-                inorder(root);
+                BST_inorder(root);
                 printf("\n");
                 
                 break;
             case 'p':
-                preorder(root);
+                BST_preorder(root);
                 printf("\n");
 
                 break;
            
            
             case 't':
-                postorder(root);
+                BST_postorder(root);
                 printf("\n");
                 break;
 
@@ -514,4 +514,3 @@ int main()
     }
 	return 0;
 }
-
