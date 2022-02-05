d48fa5a18a889fb10faa4f64c767f1c8d3f83ff4
Amal Prakash <44752430+amalp12@users.noreply.github.com>
2022-02-05 12:12:49 +0300

    Added GCD non recursive in C

--- C/gcd.c
+++ C/gcd.c
@@ -1,9 +1,22 @@
 
 
-
+// recursive 
 int gcd(int m,int n)
 {
     m = abs(m); n = abs(n);
     if (n==0) return m;
     else return gcd(n, m%n);
+}
+
+// non recursive
+int gcd(int a, int b)
+{
+    int c;
+    while (b != 0)
+    {
+        c = a % b;
+        a = b;
+        b = c;
+    }
+    return a;
 }
