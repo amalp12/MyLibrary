e77e7c899f1cb5e98d2f0d55dc31376a9f93ece9
Amal Prakash <44752430+amalp12@users.noreply.github.com>
2022-02-19 22:01:31 +0300

    Update run_nasm.sh

--- NASM/run_nasm.sh
+++ NASM/run_nasm.sh
@@ -2,8 +2,8 @@
 # it can be found by typing which bash in the terminal
 #! /usr/bin/bash
 
-read -p "Enter the path to your NASM File : " PATH
-
+#read -p "Enter the path to your NASM File : " PATH
+PATH=$1
 # replace /usr/bin/nasm with the path to your nasm executable
 # it can be found by typing which nasm in the terminal
 /usr/bin/nasm -f elf32 ${PATH}.asm -o ${PATH}.o
@@ -15,3 +15,5 @@ read -p "Enter the path to your NASM File : " PATH
 ./${PATH}
 
 exit
+
+# How to use? Put this file in the working directory and then , just type ./run_nasm.sh <Name of your .asm file without the .asm extention> im the terminal
