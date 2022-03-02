54f45e6d60cb22ac27590f7a42b0b313ab3e6558
Amal Prakash <44752430+amalp12@users.noreply.github.com>
2022-03-02 11:02:44 +0300

    Update AVLTree.c

--- C/DataStructures/AVL Tree/AVLTree.c
+++ C/DataStructures/AVL Tree/AVLTree.c
@@ -13,6 +13,7 @@ struct BST_node
     BST_node_ptr parent;
     BST_node_ptr left; // left child less than data
     BST_node_ptr right; // right child greater than data
+   
 };
 
 
@@ -102,135 +103,85 @@ BST_node_ptr BST_search(BST_node_ptr  root, int key)
     }
 
 }
-BST_node_ptr BST_get_parent( BST_node_ptr root, BST_node_ptr child)
-{
-    // if the root is empty then return NULL
-    if(root == NULL)
-    {
-        return NULL;
-    }
-    // if the root is equal to the child then return NULL
-    if(root == child)
-    {
-        return NULL;
-    }
-    // if the left child is equal to the child then return the root
-    if(root->left == child)
-    {
-        return root;
-    }
-    // if the right child is equal to the child then return the root
-    else if(root->right == child)
-    {
-        return root;
-    }
-    // if the left child is not empty then recursively call the function get parent towards left
-    if(child->data <root->data )
-    {
-        return BST_get_parent(root->left, child);
-    }
-    // if the right child is not empty then recursively call the function get parent towards right
-    else if(child->data >root->data)
-    {
-        return BST_get_parent(root->right, child);
-    }
-    
-}
 
-BST_node_ptr BST_get_sucessor(BST_node_ptr root, int data)
+
+BST_node_ptr BST_get_sucessor (BST_node_ptr root, int data)
 {
 
     BST_node_ptr search_result = BST_search(root, data);
     if(search_result!=NULL)
     {
-        // if right subtree not null then  sucessor will be the leftmost child of hte right subtree or right child itself
+        // if  rights subtree is not empty then return the minimum value of the right subtree
         if(search_result->right != NULL)
         {
-            BST_node_ptr temp = search_result->right; ;
+            BST_node_ptr temp = search_result->right;
             while(temp->left != NULL)
             {
                 temp = temp->left;
             }
-            if (temp->data < search_result->data) return NULL;
             return temp;
         }
-        else // if right subtree is null sucessor will be the last left child's parent while searching the current node
+
+        // if the right subtree is null then the sucessors lies above in among the parents 
+        // travel up using parent pointer until we see a node which is left child of parent
+        BST_node_ptr temp = search_result->parent;
+        while(temp->parent != NULL)
         {
-            BST_node_ptr sucessor= root;
-            BST_node_ptr temp = root;
-            while(temp->data != search_result->data)
+            if(temp->parent->left == temp)
             {
-                if (search_result->data > temp->data)
-                {
-                    temp = temp->right;
-                }
-                else
-                {
-                    sucessor = temp;
-                    temp = temp->left;
-                }
+                return temp->parent;
             }
-            if (sucessor->data < search_result->data) return NULL;
-            return sucessor;
-            
+            temp = temp->parent;
         }
+        return temp;
+
+        
     }
     else
     {
         return NULL;
     }
-
 }
 
 
-
-BST_node_ptr BST_get_predecessor(BST_node_ptr root, int data)
+BST_node_ptr BST_get_predecessor (BST_node_ptr root, int data)
 {
-  
-
 
     BST_node_ptr search_result = BST_search(root, data);
     if(search_result!=NULL)
     {
-        // if left subtree not null then  sucessor will be the rightmost child of hte left subtree or left child itself
+        // if  left subtree is not empty then return the maximum value of the left subtree
         if(search_result->left != NULL)
         {
-            BST_node_ptr temp = search_result->left; 
+            BST_node_ptr temp = search_result->left;
             while(temp->right != NULL)
             {
                 temp = temp->right;
             }
-            if (temp->data > search_result->data) return NULL;
             return temp;
         }
-        else // if left subtree is null sucessor will be the last right child's parent while searching the current node
+
+        // if the left subtree is null then the predecessor lies above in among the parents 
+        // travel up using parent pointer until we see a node which is right child of parent
+        BST_node_ptr temp = search_result->parent;
+        while(temp->parent != NULL)
         {
-            BST_node_ptr predecessor = root;
-            BST_node_ptr temp = root;
-            while(temp->data != search_result->data)
+            if(temp->parent->right == temp)
             {
-                if (search_result->data < temp->data)
-                {
-                    temp = temp->left;
-                }
-                else
-                {
-                    predecessor = temp;
-                    temp = temp->right;
-                }
-               
+                return temp->parent;
             }
-            if (predecessor->data > search_result->data) return NULL;
-            return predecessor;
-            
+            temp = temp->parent;
         }
+        return temp;
+
+        
     }
     else
     {
         return NULL;
     }
-
 }
+
 
 void BST_delete_node(BST_node_ptr * root, int key)
 {
@@ -248,7 +199,7 @@ void BST_delete_node(BST_node_ptr * root, int key)
     BST_node_ptr search_result = BST_search(*root, key);
     if(search_result != NULL)
     {
-        BST_node_ptr parent = BST_get_parent(*root, search_result);
+        BST_node_ptr parent = search_result->parent;
 
         // if parent is null then that means root node is to be deleted
         if (parent==NULL)
@@ -465,6 +416,18 @@ int BST_height(BST_node_ptr root)
         }
     }
 }
+int  getBalanceFactor(BST_node_ptr current)
+{
+   
+    
+    int left_height = BST_height(current->left);
+    int right_height = BST_height(current->right);
+    int balance_factor = left_height - right_height;
+    return balance_factor;
+ 
+   
+}
+
 
 void  getBalance(BST_node_ptr root, int key)
 {
@@ -500,7 +463,6 @@ following order
 So BST property is not violated anywhere.
 */
 
-
 BST_node_ptr rightRotate(BST_node_ptr y)
 {
     // root is y
@@ -528,6 +490,18 @@ BST_node_ptr rightRotate(BST_node_ptr y)
         x->right = y;
 
 
+        // if the parent has has parent
+        if(x->parent!=NULL)
+        {
+            if(x->parent->left == y)
+            {
+                x->parent->left = x;
+            }
+            else if(x->parent->right == y)
+            {
+                x->parent->right = x;
+            }
+        }
 
 
     }
@@ -566,6 +540,19 @@ BST_node_ptr leftRotate(BST_node_ptr x)
         x->parent = y;
         y->left = x;
 
+        // if the parent has a parent
+        if(y->parent!=NULL)
+        {
+            if(y->parent->left == x)
+            {
+                y->parent->left = y;
+            }
+            else
+            {
+                y->parent->right = y;
+            }
+        }
+
 
     }
     else
@@ -576,72 +563,98 @@ BST_node_ptr leftRotate(BST_node_ptr x)
     return y;
 }
 
-
-BST_node_ptr AVL_Tree_insert(BST_node_ptr  root, int key)
+BST_node_ptr AVL_Tree_Insert_Fix(BST_node_ptr new_node)
 {
+    BST_node_ptr temp_root  = new_node;
+    while(temp_root->parent!=NULL)
+    {
+        temp_root = temp_root->parent;
+    }
 
-    if(root ==NULL)
-    {
-        BST_node_ptr new_node = BST_create_node(key);
-        return new_node;
-    }
-    if(root->data > key)
-    {
-        root->left = AVL_Tree_insert(root->left, key);
-    }
-    else if (root->data < key)
-    {
-        root->right = AVL_Tree_insert(root->right, key);
-    }
-    else return root;
-    
     int balance_factor; 
-    int flag = 0;
-    if  (root!=NULL) 
+        
+    do
     {
-        if (root->data == (root)->data) flag = 1;
-        balance_factor = BST_height(root->left) - BST_height(root->right);
-        
-        if(balance_factor>1) // left child of root is heavier 
+        // calculating balace factor
+        balance_factor = BST_height(new_node->left) - BST_height(new_node->right);
+        // if the new node's parent is the left child of its parent (grandparent of new node) three cases arise
+       
+        // right chlld is heavier than left child
+        if(balance_factor <-1)
         {
-            if(root->left!=NULL &&  root->left->data  > key)//LL
+            if(new_node->right != NULL)
             {
-                return  rightRotate(root);
-            }
-            else // LR
-            {
-                root->left = leftRotate(root->left);
-                return  rightRotate(root);
+                if(new_node->right->left != NULL)
+                {
+                    rightRotate(new_node->right);
+                }
+                
+                new_node = leftRotate(new_node);
+                
             }
 
+        }
+        
+
+        // right chlld is heavier than left child
+        else if (balance_factor>1)
+        {
+                if(new_node->left != NULL)
+            {
+                if(new_node->left->right != NULL)
+                {
+                    leftRotate(new_node->left);
+                }
+                
+                new_node = rightRotate(new_node);
+                
+            }
+        }
+           
          
-     
-        }
-        else if(balance_factor<-1) // right  child of root is heavier 
-        {
-            if(root->right!=NULL &&  root->right->data < key) //RR
-            {
-                return  leftRotate(root);
-            }
-            else // RL
-            {
-                root ->right = rightRotate(root->right);
-                return  leftRotate(root);
-            }
-     
-        }
-        return root;
         
+        new_node = new_node->parent;
 
         
-        
-        
-        
-    }
-    
+    }while(new_node->parent !=NULL );
+    return new_node;   
 }
+BST_node_ptr Insert( BST_node_ptr node , int key)
+{
+    // Normal BST insertion
+    if (node == NULL)
+        return(BST_create_node(key));
+ 
+    if (key < node->data)
+        node->left  = Insert(node->left, key);
+    else if (key > node->data)
+        node->right = Insert(node->right, key);
+    else // Equal keys are not allowed in BST
+        return node;
+ 
 
-
+    int balance = getBalanceFactor(node);
+ 
+    // If this node becomes unbalanced, then balamce it
+ 
+    
+    if (balance > 1 )
+    {
+        if(getBalanceFactor(node->left) < 0) node->left =  leftRotate(node->left);
+        return rightRotate(node);
+    }
+ 
+   
+    if (balance < -1  )
+    {
+        if(getBalanceFactor(node->right) > 0) node->right = rightRotate(node->right);
+        return leftRotate(node);
+    }
+ 
+ 
+    /* return the (unchanged) node pointer */
+    return node;
+}
 // parenthesis notation code start
 
 void parenthesisNotation(BST_node_ptr root)
@@ -677,9 +690,112 @@ void parenthesisNotation(BST_node_ptr root)
     
 }
 
+BST_node_ptr Search(BST_node_ptr root, int key)
+{
+    return BST_search(root, key);
+}
 // parenthesis notation code end
 
 
+
+BST_node_ptr minValueNode(BST_node_ptr node)
+{
+    BST_node_ptr current = node;
+ 
+    /* loop down to find the leftmost leaf */
+    while (current->left != NULL)
+        current = current->left;
+ 
+    return current;
+}
+
+BST_node_ptr maxValueNode(BST_node_ptr node)
+{
+    BST_node_ptr current = node;
+ 
+    /* loop down to find the leftmost leaf */
+    while (current->right != NULL)
+        current = current->right;
+ 
+    return current;
+}
+BST_node_ptr AVL_Tree_Delete(BST_node_ptr root, int key){
+    
+    
+    // Normall BST deletion
+ 
+    if (root == NULL)
+        return root;
+ 
+    
+    if ( key < root->data )
+        root->left = AVL_Tree_Delete(root->left, key);
+ 
+    
+    else if( key > root->data )
+        root->right = AVL_Tree_Delete(root->right, key);
+ 
+    // found the node
+    else
+    {
+        // node with only one child or 
+        if( (root->left == NULL) || (root->right == NULL) )
+        {
+            BST_node_ptr temp = root->left ;
+            if (temp == NULL)
+            {
+                temp = root->right;
+            }
+            // No child case
+            if (temp == NULL)
+            {
+                temp = root;
+                root = NULL;
+            }
+            else // One child case
+            *root = *temp;
+            free(temp);
+        }
+        else
+        {
+            // node with two children: Get the inorder
+            // successor (smallest in the right subtree)
+            BST_node_ptr temp = maxValueNode(root->left);
+ 
+            // Copy the inorder successor's data to this node
+            root->data = temp->data;
+ 
+            // Delete the inorder successor
+            root->left = AVL_Tree_Delete(root->left, temp->data);
+        }
+    }
+ 
+    // If the tree had only one node then return
+    if (root == NULL)
+      return root;
+ 
+    
+    int balance = getBalanceFactor(root);
+ 
+    
+ 
+    
+    if (balance > 1 )
+    {
+        if(getBalanceFactor(root->left) < 0) root->left =  leftRotate(root->left);
+        return rightRotate(root);
+    }
+ 
+   
+    if (balance < -1  )
+    {
+        if(getBalanceFactor(root->right) > 0) root->right = rightRotate(root->right);
+        return leftRotate(root);
+    }
+ 
+    return root;
+   
+}
 
 void PrintTree(BST_node_ptr root)
 {
@@ -687,25 +803,20 @@ void PrintTree(BST_node_ptr root)
     printf("\n");
 }
 
-int BST_isAVLTree(BST_node_ptr root)
+void DeleteNode(BST_node_ptr * root, int key)
 {
-    if(root == NULL)
+    BST_node_ptr search_result = BST_search(*root, key);
+    if (search_result!=NULL)
     {
-        return 1;
-    }
-    int left_height = BST_height(root->left);
-    int right_height = BST_height(root->right);
-    if(abs(left_height - right_height) <= 1)
-    {
-        return isAVL(root->left) && isAVL(root->right);
+        printf("%d\n", search_result->data);
+        (*root)=AVL_Tree_Delete(*root, key);
     }
     else
     {
-        return 0;
+        printf("FALSE\n");
     }
+    
 }
-
-
 int main()
 {
     char in; int data;
@@ -719,17 +830,17 @@ int main()
             case 'i':
 
                 scanf("%d", &data);
-                root =AVL_Tree_insert(root, data);
+                root = Insert(root, data);
                 break;
             
             case 'd':
                 scanf("%d", &data);
-                BST_delete(&root, data);
+                DeleteNode(&root, data);
                 break;
 
             case 's':
                 scanf("%d", &data);
-                BST_node_ptr search_result= BST_search(root, data);
+                BST_node_ptr search_result= Search(root, data);
                 if (search_result != NULL)
                 {
                     printf("TRUE\n");
@@ -739,7 +850,7 @@ int main()
                     printf("FALSE\n");
                 }
                 break;
-
+    
             case 'b':
                 scanf("%d", &data);
                 getBalance(root, data);
